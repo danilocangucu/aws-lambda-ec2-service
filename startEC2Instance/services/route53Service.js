5c8872577e5b6c6750bed2bd59c210b4af7a1fa5
Danilo Canguçu <daniloccangucu@gmail.com>
2024-09-26 14:00:59 +0100

    added TODOs

--- startEC2Instance/services/route53Service.js
+++ startEC2Instance/services/route53Service.js
@@ -2,9 +2,11 @@ const AWS = require("aws-sdk");
 const route53 = new AWS.Route53();
 
 const hostedZoneId = process.env.HOSTED_ZONE_ID;
+// TODO get the DNS name from the event via keyName
 const dnsName = process.env.DNS_NAME;
 
 async function updateRoute53(publicIp) {
+  // TODO get the DNS name from the event
   const route53Params = {
     HostedZoneId: hostedZoneId,
     ChangeBatch: {
