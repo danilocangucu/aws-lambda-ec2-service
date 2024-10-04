d8f90c485bf5feaf09fef7ddea25974ca4f7e2fd
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-04 16:45:21 +0200

    imported new functions and improved error handling

--- startEC2Instance/src/index.ts
+++ startEC2Instance/src/index.ts
@@ -1,33 +1,43 @@
 import {
   startEC2Instance,
   tagEC2Instance,
-  getPublicIP,
   findEC2Instance,
 } from "./services/ec2Service";
 import { updateRoute53 } from "./services/route53Service";
 import * as dotenv from "dotenv";
-import { getAmiKeyPairs } from "./utils/dotenv";
+import { getEnvVariable, parseAmiKeyPairs } from "./utils/dotenv";
 import { validate, eventSchema } from "./utils/validators";
 import {
   convertAWSTypeToLocalEC2Instance,
   matchAmiKeyPair,
+  waitForInstanceReady,
 } from "./utils/ec2Utils";
-import { EC2Instance } from "./types/ec2Types";
+import { EventPayload } from "./types/utilTypes";
 
 dotenv.config();
 
-exports.handler = async (event: any) => {
+exports.handler = async (event: EventPayload) => {
   try {
     validate(event, eventSchema);
 
     const { amiKeyPair, instanceType } = event;
 
-    const amiKeyPairs = getAmiKeyPairs("AMI_KEY_PAIRS");
+    const amiKeyPairsString = getEnvVariable("AMI_KEY_PAIRS");
+    if (!amiKeyPairsString) {
+      throw new Error("AMI_KEY_PAIRS is not defined in environment variables.");
+    }
+
+    const amiKeyPairs = parseAmiKeyPairs(amiKeyPairsString);
+    if (!amiKeyPairs) {
+      throw new Error("Invalid format for AMI key pairs.");
+    }
 
     const matchedKeyPair = matchAmiKeyPair(amiKeyPairs, amiKeyPair);
+    if (!matchedKeyPair) {
+      throw new Error("AMI key pair not found.");
+    }
 
     const foundEC2Instance = await findEC2Instance(matchedKeyPair);
-
     if (foundEC2Instance) {
       console.log(
         `Instance already running: ${foundEC2Instance.instanceId} with IP: ${foundEC2Instance.publicIp}`
@@ -42,34 +52,34 @@ exports.handler = async (event: any) => {
       matchedKeyPair,
       instanceType
     );
-
     if (!ec2InstanceResponse) {
       throw new Error("Failed to start EC2 instance.");
     }
 
-    const createdInstance: EC2Instance =
+    const createdInstance =
       convertAWSTypeToLocalEC2Instance(ec2InstanceResponse);
-
-    console.log(
-      `Successfully started EC2 instance: ${createdInstance.instanceId}`
-    );
-
-    await tagEC2Instance(createdInstance);
-
-    const publicIp = await getPublicIP(createdInstance);
-
-    if (!publicIp) {
-      throw new Error("Failed to get public IP address.");
+    if (!createdInstance) {
+      throw new Error("Failed to convert EC2 instance response.");
     }
 
-    console.log(`Public IP of the instance: ${publicIp}`);
+    const isTagged = await tagEC2Instance(createdInstance);
+    if (!isTagged) {
+      throw new Error("Failed to tag EC2 instance.");
+    }
 
-    await updateRoute53(publicIp);
-    console.log("Updated Route 53 with new IP address.");
+    const readyInstance = await waitForInstanceReady(createdInstance);
+    if (!readyInstance) {
+      throw new Error("Failed to wait for EC2 instance to be ready.");
+    }
+
+    const isUpdated = await updateRoute53(readyInstance);
+    if (!isUpdated) {
+      throw new Error("Failed to update Route 53 with the new IP address.");
+    }
 
     return {
       statusCode: 200,
-      body: `EC2 instance launched: ${createdInstance.instanceId} with IP: ${publicIp}`,
+      body: `EC2 instance launched: ${readyInstance.instanceId} with IP: ${readyInstance.publicIp}`,
     };
   } catch (error) {
     console.error("Error:", error);
