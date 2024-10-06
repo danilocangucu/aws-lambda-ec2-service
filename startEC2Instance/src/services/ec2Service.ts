6c6401c7a605306c3b66c7182d39e7ca25cda29b
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-06 17:29:09 +0200

    added instance launch and handle[State] functions

--- startEC2Instance/src/services/ec2Service.ts
+++ startEC2Instance/src/services/ec2Service.ts
@@ -2,11 +2,15 @@ import AWS, { EC2 } from "aws-sdk";
 import dotenv from "dotenv";
 import {
   AmiKeyPair,
-  EC2Instance,
-  EC2InstanceStatus,
+  Instance,
+  InstanceState,
   InstanceType,
 } from "../types/ec2Types";
-import { convertAWSTypeToLocalEC2Instance } from "../utils/ec2Utils";
+import {
+  convertAWSTypeToLocalEC2Instance,
+  waitForInstanceReady,
+} from "../utils/ec2Utils";
+import { updateRoute53 } from "./route53Service";
 
 dotenv.config();
 
@@ -14,16 +18,16 @@ const ec2 = new AWS.EC2({ region: "eu-north-1" });
 
 export async function findEC2Instance(
   amiKeyPair: AmiKeyPair,
-  ec2InstanceStatus?: EC2InstanceStatus[]
-): Promise<EC2Instance | null> {
+  ec2InstanceStatus?: InstanceState[]
+): Promise<Instance | null> {
   const params = {
     Filters: [
       { Name: "image-id", Values: [amiKeyPair.amiId] },
       {
         Name: "instance-state-name",
         Values: ec2InstanceStatus || [
-          EC2InstanceStatus.RUNNING,
-          EC2InstanceStatus.PENDING,
+          InstanceState.RUNNING,
+          InstanceState.PENDING,
         ],
       },
       { Name: "key-name", Values: [amiKeyPair.keyName] },
@@ -43,7 +47,110 @@ export async function findEC2Instance(
   return null;
 }
 
-export async function startEC2Instance(
+export async function handleInstanceState(
+  foundEC2Instance: Instance | null,
+  instanceState: InstanceState,
+  amiKeyPair: AmiKeyPair,
+  instanceType: InstanceType
+) {
+  if (foundEC2Instance) {
+    if (instanceState === InstanceState.START) {
+      return handleInstanceAlreadyRunning(foundEC2Instance);
+    } else if (instanceState === InstanceState.STOP) {
+      return handleStopInstance(foundEC2Instance);
+    }
+  } else if (!foundEC2Instance && instanceState === InstanceState.START) {
+    return await handleStartNewInstance(amiKeyPair, instanceType);
+  }
+  return {
+    statusCode: 400,
+    body: "Invalid instance state or instance not found.",
+  };
+}
+
+function handleInstanceAlreadyRunning(foundEC2Instance: Instance) {
+  console.log(
+    `Instance already running: ${foundEC2Instance.instanceId} with IP: ${foundEC2Instance.publicIp}`
+  );
+  return {
+    statusCode: 204,
+    body: `Instance already running: ${foundEC2Instance.instanceId} with IP: ${foundEC2Instance.publicIp}`,
+  };
+}
+
+async function handleStopInstance(foundEC2Instance: Instance) {
+  console.log("Stopping EC2 instance...");
+  // TODO implement stopping the instance;
+  const stopResult = true;
+  return {
+    statusCode: stopResult ? 200 : 500,
+    body: stopResult
+      ? "Instance stopped successfully."
+      : "Failed to stop the instance.",
+  };
+}
+
+async function handleStartNewInstance(
+  amiKeyPair: AmiKeyPair,
+  instanceType: InstanceType
+) {
+  console.log("Starting new EC2 instance...");
+
+  const readyInstance = await launchAndConfigureEC2Instance(
+    amiKeyPair,
+    instanceType
+  );
+
+  if (!readyInstance) {
+    throw new Error("Failed to launch and configure the EC2 instance.");
+  }
+
+  return {
+    statusCode: 200,
+    body: `EC2 instance launched: ${readyInstance.instanceId} with IP: ${readyInstance.publicIp}`,
+  };
+}
+
+async function launchAndConfigureEC2Instance(
+  amiKeyPair: AmiKeyPair,
+  instanceType: InstanceType
+): Promise<Instance | null> {
+  console.log("Starting new EC2 instance...");
+
+  const ec2InstanceResponse = await startEC2Instance(amiKeyPair, instanceType);
+  if (!ec2InstanceResponse) {
+    console.error("Failed to start EC2 instance.");
+    return null;
+  }
+
+  const createdInstance = convertAWSTypeToLocalEC2Instance(ec2InstanceResponse);
+  if (!createdInstance) {
+    console.error("Failed to convert EC2 instance response.");
+    return null;
+  }
+
+  const isTagged = await tagEC2Instance(createdInstance);
+  if (!isTagged) {
+    console.error("Failed to tag EC2 instance.");
+    return null;
+  }
+
+  const readyInstance = await waitForInstanceReady(createdInstance);
+  if (!readyInstance) {
+    console.error("Failed to wait for EC2 instance to be ready.");
+    return null;
+  }
+
+  const isUpdated = await updateRoute53(readyInstance);
+  if (!isUpdated) {
+    console.error("Failed to update Route 53 with the new IP address.");
+    return null;
+  }
+
+  return readyInstance;
+}
+
+async function startEC2Instance(
   amiKeyPair: AmiKeyPair,
   instanceType: InstanceType
 ) {
@@ -62,9 +169,7 @@ export async function startEC2Instance(
   return result.Instances?.[0];
 }
 
-export async function tagEC2Instance(
-  ec2Instance: EC2Instance
-): Promise<boolean> {
+async function tagEC2Instance(ec2Instance: Instance): Promise<boolean> {
   if (!ec2Instance.instanceId) {
     console.error("EC2 instance ID is missing.");
     return false;
